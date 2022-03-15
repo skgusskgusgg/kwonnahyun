5348af442b0d9c9ba11cb46d616b599dc3a4475d
kwonnahyun <92068041+skgusskgusgg@users.noreply.github.com>
2022-03-15 13:21:51 +0900

    Add files via upload

---
+++ subpage/leweekend.js
@@ -0,0 +1,10 @@
+
+// const video = document.querySelectorAll('.video');
+// for (let item of video) {
+//     item.addEventListener('mouseenter', (e) => {
+//         e.currentTarget.querySelector('video').play();
+//     });
+//     item.addEventListener('mouseleave', (e) => {
+//         e.currentTarget.querySelector('video').pause();
+//     });
+// };
---
+++ subpage/revation.js
@@ -0,0 +1,9 @@
+const video = document.querySelectorAll('.video');
+for (let item of video) {
+    item.addEventListener('mouseenter', (e) => {
+        e.currentTarget.querySelector('video').play();
+    });
+    item.addEventListener('mouseleave', (e) => {
+        e.currentTarget.querySelector('video').pause();
+    });
+}; 
